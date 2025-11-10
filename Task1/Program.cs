d6fe6024157f6f7b53a8b43e7be671264a049f68
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-11-10 09:12:26 +0545

    workshop 2

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an object of Operators class
+        Operators op = new Operators();
+
+        // Call all methods
+        op.Add(10, 5);
+        op.Subtract(10, 5);
+        op.Multiply(10, 5);
+        op.Divide(10, 5);
+
+        // Check Odd or Even
+        op.OddEvenFinder(7);
+        op.OddEvenFinder(12);
+    }
+}
